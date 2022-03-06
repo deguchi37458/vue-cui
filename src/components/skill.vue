1beb452bb7ee669f57d94d112ad6cb6f9d98d9aa
deguchi <deguchi37458@gmail.com>
2022-03-06 17:51:35 +0900

    update skill

--- src/components/skill.vue
+++ src/components/skill.vue
@@ -3,7 +3,7 @@
         <ul>
             <li><dl><dt>OS</dt><dd>&nbsp;：Windows, Linux</dd></dl></li>
             <li><dl><dt>使用言語</dt><dd>&nbsp;：HTML/CSS, JavaScript, PHP</dd></dl></li>
-            <li><dl><dt>フレームワーク</dt><dd>：Vue.js</dd></dl></li>
+            <li><dl><dt>フレームワーク</dt><dd>&nbsp;：Vue.js</dd></dl></li>
             <li><dl><dt>CMS</dt><dd>&nbsp;：WordPress</dd></dl></li>
             <li><dl><dt>ツール</dt><dd>&nbsp;：npm</dd></dl></li>
             <li><dl><dt>開発環境</dt><dd>&nbsp;：Docker, XAMPP(MAMP)</dd></dl></li>
